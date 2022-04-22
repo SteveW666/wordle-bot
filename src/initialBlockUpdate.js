fb881bdf7a129657320ffd21415f43ea6cd14f28
Eric Wasylishen <ewasylishen@gmail.com>
2022-04-21 20:35:35 -0600

    Fix compareWords('teeth', 'tepee')

--- src/initialBlockUpdate.js
+++ src/initialBlockUpdate.js
@@ -13,27 +13,31 @@ function letterYellow(answer, letter, usedAnswerIndices) {
   return [false, -1];
 };
 
-//checks for green block
-function letterGreen(answer, letter, pos) {
-  if (answer[pos] === letter) {
-    return 2;
-  }
-  else return 0;
-};
-
+/**
+ * Returns a coloring for `guess` assuming a particular answer.
+ */
 function compareWords(guess, answer) {
-  let valBlock = [0, 0, 0, 0, 0];
+  let valBlock = new Array();
+  valBlock.length = guess.length;
+  valBlock.fill(0)
+
   let usedAnswerIndices = new Set();
 
+  // assign greens
   for (let i = 0; i < answer.length; i++) {
-    //green block
-    if (letterGreen(answer, guess[i], i)) {
+    if (guess[i] === answer[i]) {
       valBlock[i] = 2;
       usedAnswerIndices.add(i);
       // console.log("inside green", usedAnswerIndices);
     }
   }
+
+  // assign yellows
   for (let i = 0; i < answer.length; i++) {
+    if (valBlock[i] != 0) {
+      continue; // already colored in
+    }
+
     const [isYellow, indexUsedInAnswer] = letterYellow(answer, guess[i], usedAnswerIndices);
     if (isYellow) {
       //yellow block
@@ -46,7 +50,9 @@ function compareWords(guess, answer) {
   return valBlock;
 };
 
-// console.log(compareWords('teeth', 'three'));
-// console.log(compareWords('tooth', 'trope'));
+// TODO: investigate moving these to an executable unit test, e.g. https://jestjs.io/docs/getting-started
+// console.log(compareWords('teeth', 'three')); // expected 2, 1, 1, 0, 1
+// console.log(compareWords('tooth', 'trope')); // expected 2, 0, 2, 0, 0
+// console.log(compareWords('teeth', 'tepee')); // expected 2, 2, 1, 0, 0
 
 module.exports = { compareWords };
